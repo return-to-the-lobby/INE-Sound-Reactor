75721f06a66465ffa736339cd7264aeed66b6a96
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2022-12-19 22:29:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import eel
+import retry
+import random
+
+@retry.retry(OSError)
+def start():
+    port = random.randint(80, 65535)
+    return eel.start('index.html', port=port)
 
 eel.init('scripts')
-eel.start('index.html')
+start()
