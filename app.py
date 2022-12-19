b62f6dd5267a61d5a0171dbd64eae23778f29303
NO-TORY <97148865+NO-TORY@users.noreply.github.com>
2022-12-19 22:37:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import os
+import sys
 import eel
 import retry
 import random
+
+pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
 
 @retry.retry(OSError)
 def start():
     port = random.randint(80, 65535)
     return eel.start('index.html', port=port)
 
-eel.init('scripts')
+eel.init(os.path.join(sys._MEIPASS, 'scripts') if pyinstaller else 'scripts')
 start()
